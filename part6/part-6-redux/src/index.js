29ae2b09f343c793f19f8feaccf7af9a4213652c
andrewtioo <andrew.prasetyo@wahyoo.com>
2023-03-15 21:21:01 +0700

    Exercise 6.3

--- part6/part-6-redux/src/index.js
+++ part6/part-6-redux/src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import noteReducer from "./reducers/noteReducer";
-
+import { Provider } from "react-redux";
 import { createStore } from "redux";
+
+import App from "./App";
 
 // const counterReducer = (state = 0, action) => {
 // if (action.type === "INCREMENT") {
@@ -78,21 +80,11 @@ store.dispatch({
   },
 });
 
-const App = () => {
-  return (
-    <div>
-      <ul>
-        {store.getState().map((note) => (
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? "important" : ""}</strong>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 const renderApp = () => {
   root.render(<App />);
